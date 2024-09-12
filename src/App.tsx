704a82caf5ff5f388a1d531e29a7960fa0e8e0e4
haalmarc <marcus-haa@hotmail.com>
2024-09-12 20:05:48 +0200

    refactor: clean up app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Oppgave1 } from "./oppgaver/Oppgave1";
 import { Oppgave2 } from "./oppgaver/Oppgave2";
 import { Oppgave3 } from "./oppgaver/Oppgave3";
+import { Layout } from "./components/Layout";
+import { NoMatch } from "./components/NoMatch";
 
 export function App() {
   return (
@@ -17,47 +19,6 @@ export function App() {
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
-    </div>
-  );
-}
-
-function Layout() {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/opg1">Oppgave 1</Link>
-          </li>
-          <li>
-            <Link to="/opg2">Oppgave 2</Link>
-          </li>
-          <li>
-            <Link to="/opg3">Oppgave 3</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
-      <Outlet />
-    </div>
-  );
-}
-
-function NoMatch() {
-  return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
     </div>
   );
 }
---
+++ src/components/Layout.tsx
@@ -0,0 +1,28 @@
+import { Outlet, Link } from "react-router-dom";
+
+export function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Oppgave 1</Link>
+          </li>
+          <li>
+            <Link to="/opg2">Oppgave 2</Link>
+          </li>
+          <li>
+            <Link to="/opg3">Oppgave 3</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <Outlet />
+    </div>
+  );
+}
